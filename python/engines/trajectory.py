e91fbe173e4d0b5e1a475c71228fe18a71a74d34
Jan C Wynholds <jcwynholds@gmail.com>
2013-03-01 09:03:58 -0800

    Update trajectory.py
    radians vs degrees.

--- python/engines/trajectory.py
+++ python/engines/trajectory.py
@@ -1,12 +1,14 @@
 from math import cos, sin, sqrt, asin, pow
 
 gravity = 9.81
+def get_radians(angle):
+	return (angle * math.pi)/180
 
 def get_distance_traveled(velocity, height, angle):
-	return velocity * cos(angle) / gravity * (velocity * sin(angle) + sqrt(velocity * sin(angle) + 2 * gravity * height))
+	return velocity * cos(get_radians(angle)) / gravity * (velocity * sin(get_radians(angle)) + sqrt(velocity * sin(get_radians(angle)) + 2 * gravity * height))
 
 def time_of_flight(distance, velocity, angle):
-	return distance / velocity * cos(angle)
+	return distance / velocity * cos(get_radians(angle))
 
 def angle_of_reach(distance, velocity):
 	m = (1/2) * (asin((gravity*distance) / (pow(velocity, 2))))
